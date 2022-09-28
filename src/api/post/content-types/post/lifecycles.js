2939e0bb0fc35e3c81786ad2cf9b6e49762a661c
hayhaydz <haydon1900@gmail.com>
2022-09-28 17:08:20 +0100

    Following Strapi and Astro tutorial

---
+++ src/api/post/content-types/post/lifecycles.js
@@ -0,0 +1,15 @@
+const readingTime = require("reading-time");
+/**
+ * Read the documentation (https://docs.strapi.io/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
+ * to customize this model
+ */
+module.exports = {
+    async beforeCreate(event) {
+        const { data } = event.params;
+        data.readingTime = readingTime(data.content)?.text || null;
+    },
+    async beforeUpdate(event) {
+        const { data } = event.params;
+        data.readingTime = readingTime(data.content)?.text || null;
+    },
+};
